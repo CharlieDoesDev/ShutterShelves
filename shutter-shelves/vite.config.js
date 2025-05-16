203d87a45ca79e337dad899860cb72ed4aba9468
simonswartout <121693487+simonswartout@users.noreply.github.com>
2025-05-15 23:02:38 -0400

    build updates

--- shutter-shelves/vite.config.js
+++ shutter-shelves/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.production' });
 
 export default defineConfig({
   base: '/YOUR_REPO_NAME/',
