3e6668625ae3d45609e79d0b896e3a1d97d648f4
simonswartout <121693487+simonswartout@users.noreply.github.com>
2025-05-16 00:06:28 -0400

    updates

--- shutter-shelves/src/main.jsx
+++ shutter-shelves/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './components/index.css';
-import './components/styles.css';
+import './index.css';
+import './styles.css';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
