2b7bd7966b1195aab64ee586b6ce6d3cd1433f61
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2025-05-31 08:06:41 -0400

    Update ProcessingWindow.jsx

--- shutter-shelves/src/components/AppStates/ProcessingWindow.jsx
+++ shutter-shelves/src/components/AppStates/ProcessingWindow.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import CenterPanel from "../SimpleContainers/CenterPanel";
 import "./ProcessingWindow.css";
 import { asyncProgressBar } from "../../lib/Util.js";
 
